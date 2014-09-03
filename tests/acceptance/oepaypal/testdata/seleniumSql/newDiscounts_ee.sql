53c74753620c02b1ede15ed83fef158fa6e3788f
shiftas <Shiftass@gmail.com>
2014-09-03 09:47:01 +0300

    Fixing paypal demo data for EE edition.

--- tests/acceptance/oepaypal/testdata/seleniumSql/newDiscounts_ee.sql
+++ tests/acceptance/oepaypal/testdata/seleniumSql/newDiscounts_ee.sql
@@ -1,11 +1,13 @@
 # Discount demo data
-INSERT INTO `oxdiscount` (`OXID`,           `OXSHOPID`, `OXSHOPINCL`, `OXSHOPEXCL`, `OXACTIVE`, `OXTITLE`,                 `OXTITLE_1`,                         `OXAMOUNT`, `OXAMOUNTTO`, `OXPRICETO`, `OXPRICE`, `OXADDSUMTYPE`, `OXADDSUM`, `OXITMARTID`, `OXITMAMOUNT`, `OXITMMULTIPLE`) VALUES
-                         ('testcatdiscount', 1,          1,            0,            1,         'discount for category', 	'discount for category',            1,            999999,       999999,      1,       'abs',   	      5,        '',             0,             0),
-                         ('discount1',	   1,          1,            0,            1,         'discount for product',     'discount from 10 till 20', 		 1,            999999,       20,          15,      '%',              2,        '',             0,             0),
-                         ('diskount2',       1,          1,            0,            1,         '1 DE test discount',       'discount from 20 till 50',   	 1,            999999,       50,          20,      '%',              5,         '',            0,             0),
-			    ('diskount3',  	   1,          1,            0,            1,         '1 DE test discount',       'discount from 50 till 999',    	 1,            999999,       999,         50,      'abs',            5,         '',            0,             0),
-			    ('itmdiscount',     1,          1,            0,            1,         'Itm discount',             'Itm discount',         		 1,            999999,       0,           0,       'itm',            0,         '1001',        1,             0);
+INSERT INTO `oxdiscount` (`OXID`,           `OXMAPID`, `OXSHOPID`, `OXACTIVE`, `OXTITLE`,                 `OXTITLE_1`,                         `OXAMOUNT`, `OXAMOUNTTO`, `OXPRICETO`, `OXPRICE`, `OXADDSUMTYPE`, `OXADDSUM`, `OXITMARTID`, `OXITMAMOUNT`, `OXITMMULTIPLE`) VALUES
+                         ('testcatdiscount', 101,        1,          1,         'discount for category', 	'discount for category',            1,            999999,       999999,      1,       'abs',   	      5,        '',             0,             0),
+                         ('discount1',	     102,        1,          1,         'discount for product',     'discount from 10 till 20', 		 1,            999999,       20,          15,      '%',              2,        '',             0,             0),
+                         ('diskount2',       103,        1,          1,         '1 DE test discount',       'discount from 20 till 50',   	 1,            999999,       50,          20,      '%',              5,         '',            0,             0),
+                         ('diskount3',  	   104,        1,          1,         '1 DE test discount',       'discount from 50 till 999',    	 1,            999999,       999,         50,      'abs',            5,         '',            0,             0),
+                         ('itmdiscount',     105,        1,          1,         'Itm discount',             'Itm discount',         		 1,            999999,       0,           0,       'itm',            0,         '1001',        1,             0);
 
+REPLACE INTO `oxdiscount2shop` (`OXSHOPID`, `OXMAPOBJECTID`) VALUES
+  (1, 101), (1, 102), (1, 103), (1, 104), (1, 105);
 
 
 # object2discount

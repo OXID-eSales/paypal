697c91e2bff4e4fada8a6689003fd9e22721f633
vilmal <vilma.liorensaityte@gmail.com>
2014-10-21 13:43:57 +0300

    updated paypal version to 3.1.2

--- source/modules/oe/oepaypal/metadata.php
+++ source/modules/oe/oepaypal/metadata.php
@@ -35,7 +35,7 @@ $aModule = array(
         'en' => 'Module for PayPal payment.',
     ),
     'thumbnail'    => 'logo.jpg',
-    'version'      => '3.1.1',
+    'version'      => '3.1.2',
     'author'       => 'OXID eSales AG',
     'url'          => 'http://www.oxid-esales.com',
     'email'        => 'info@oxid-esales.com',

a31692407f9976f34fb8c623baaacc6db526b8d2
Mantas Vaitkunas <mantas.vaitkunas@oxid-esales.com>
2017-01-31 17:02:33 +0100

    ESDEV-3777 Azure theme must be active for tests

--- tests/additional.inc.php
+++ tests/additional.inc.php
@@ -1,3 +1,5 @@
 <?php
 
-oxDb::getDb()->getOne("UPDATE oxuser SET OXID='oxdefaultadmin' WHERE oxusername='admin'");
+oxDb::getDb()->execute("UPDATE oxuser SET OXID='oxdefaultadmin' WHERE oxusername='admin'");
+# Activate Azure theme
+oxDb::getDb()->execute("UPDATE `oxconfig` SET `OXVARVALUE` = 0x4db70f6d1a WHERE `OXVARNAME` = 'sTheme'");

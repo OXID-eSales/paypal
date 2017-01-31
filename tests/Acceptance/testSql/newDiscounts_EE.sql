56495b59c42525b495e83aa7908820a297417a49
Saulius Stasiukaitis <stasiukaitis.saulius@gmail.com>
2017-01-31 17:02:24 +0100

    ESDEV-3777 Fix SQL error as sorting is mandatory for discounts

--- tests/Acceptance/testSql/newDiscounts_EE.sql
+++ tests/Acceptance/testSql/newDiscounts_EE.sql
@@ -1,10 +1,10 @@
 # Discount demo data
-INSERT INTO `oxdiscount` (`OXID`,           `OXMAPID`, `OXSHOPID`, `OXACTIVE`, `OXTITLE`,                 `OXTITLE_1`,                         `OXAMOUNT`, `OXAMOUNTTO`, `OXPRICETO`, `OXPRICE`, `OXADDSUMTYPE`, `OXADDSUM`, `OXITMARTID`, `OXITMAMOUNT`, `OXITMMULTIPLE`) VALUES
-                         ('testcatdiscount', 101,        1,          1,         'discount for category', 	'discount for category',            1,            999999,       999999,      1,       'abs',   	      5,        '',             0,             0),
-                         ('discount1',	     102,        1,          1,         'discount for product',     'discount from 10 till 20', 		 1,            999999,       20,          15,      '%',              2,        '',             0,             0),
-                         ('diskount2',       103,        1,          1,         '1 DE test discount',       'discount from 20 till 50',   	 1,            999999,       50,          20,      '%',              5,         '',            0,             0),
-                         ('diskount3',  	   104,        1,          1,         '1 DE test discount',       'discount from 50 till 999',    	 1,            999999,       999,         50,      'abs',            5,         '',            0,             0),
-                         ('itmdiscount',     105,        1,          1,         'Itm discount',             'Itm discount',         		 1,            999999,       0,           0,       'itm',            0,         '1001',        1,             0);
+INSERT INTO `oxdiscount` (`OXID`,           `OXMAPID`, `OXSHOPID`, `OXACTIVE`, `OXTITLE`,                 `OXTITLE_1`,                         `OXAMOUNT`, `OXAMOUNTTO`, `OXPRICETO`, `OXPRICE`, `OXADDSUMTYPE`, `OXADDSUM`, `OXITMARTID`, `OXITMAMOUNT`, `OXITMMULTIPLE`, `OXSORT`) VALUES
+                         ('testcatdiscount', 101,        1,          1,         'discount for category', 	'discount for category',            1,            999999,       999999,      1,       'abs',   	      5,        '',             0,             0, 1),
+                         ('discount1',	     102,        1,          1,         'discount for product',     'discount from 10 till 20', 		 1,            999999,       20,          15,      '%',              2,        '',             0,             0, 2),
+                         ('diskount2',       103,        1,          1,         '1 DE test discount',       'discount from 20 till 50',   	 1,            999999,       50,          20,      '%',              5,         '',            0,             0, 3),
+                         ('diskount3',  	   104,        1,          1,         '1 DE test discount',       'discount from 50 till 999',    	 1,            999999,       999,         50,      'abs',            5,         '',            0,             0, 4),
+                         ('itmdiscount',     105,        1,          1,         'Itm discount',             'Itm discount',         		 1,            999999,       0,           0,       'itm',            0,         '1001',        1,             0, 5);
 
 REPLACE INTO `oxdiscount2shop` (`OXSHOPID`, `OXMAPOBJECTID`) VALUES
   (1, 101), (1, 102), (1, 103), (1, 104), (1, 105);

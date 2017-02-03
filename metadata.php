6d15e478a3e18d4a41752a59fcd23bd86b6d6ee9
Mantas Vaitkūnas <manvait@gmail.com>
2017-02-03 12:53:47 +0100

    Change module version to 4.0.0

--- metadata.php
+++ metadata.php
@@ -35,7 +35,7 @@ $aModule = array(
         'en' => 'Module for PayPal payment.',
     ),
     'thumbnail'    => 'logo.jpg',
-    'version'      => '3.2.0',
+    'version'      => '4.0.0',
     'author'       => 'OXID eSales AG',
     'url'          => 'http://www.oxid-esales.com',
     'email'        => 'info@oxid-esales.com',

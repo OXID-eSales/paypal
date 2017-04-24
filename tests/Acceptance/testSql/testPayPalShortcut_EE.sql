d7b835b240e5123c8e9c15cf42e6b967b3d2db3c
Mantas Vaitkunas <mantas.vaitkunas@oxid-esales.com>
2017-04-24 12:58:30 +0200

    char(32) allowed in OXID
    Issue appeared while running tests with MySQL 5.7.

--- tests/Acceptance/testSql/testPayPalShortcut_EE.sql
+++ tests/Acceptance/testSql/testPayPalShortcut_EE.sql
@@ -2,7 +2,7 @@
 DELETE FROM `oxconfig` WHERE `OXVARNAME` IN ('blOEPayPalStandardCheckout','blOEPayPalExpressCheckout','blOEPayPalECheckoutInMiniBasket','blOEPayPalECheckoutInDetails');
 
 INSERT INTO `oxconfig` (`OXID`, `OXSHOPID`, `OXMODULE`, `OXVARNAME`, `OXVARTYPE`, `OXVARVALUE`) VALUES
-('__blOEPayPalStandardCheckout', '1', 'module:oepaypal', 'blOEPayPalStandardCheckout', 'bool', ''),
-('__blOEPayPalExpressCheckout', '1', 'module:oepaypal', 'blOEPayPalExpressCheckout', 'bool', ''),
-('__blOEPayPalECheckoutInMiniBasket', '1', 'module:oepaypal', 'blOEPayPalECheckoutInMiniBasket', 'bool', ''),
-('__blOEPayPalECheckoutInDetails', '1', 'module:oepaypal', 'blOEPayPalECheckoutInDetails', 'bool', '');
+('blOEPayPalStandardCheckout', '1', 'module:oepaypal', 'blOEPayPalStandardCheckout', 'bool', ''),
+('blOEPayPalExpressCheckout', '1', 'module:oepaypal', 'blOEPayPalExpressCheckout', 'bool', ''),
+('blOEPayPalECheckoutInMiniBasket', '1', 'module:oepaypal', 'blOEPayPalECheckoutInMiniBasket', 'bool', ''),
+('blOEPayPalECheckoutInDetails', '1', 'module:oepaypal', 'blOEPayPalECheckoutInDetails', 'bool', '');

4a3836f68eab187ef158a135fcc21137fb19cc3f
hkreuter <heike.reuter@oxid-esales.com>
2017-06-01 14:52:39 +0200

    ESDEV-4276 Set module version to 5.0.0

--- metadata.php
+++ metadata.php
@@ -35,7 +35,7 @@ $aModule = array(
         'en' => 'Module for PayPal payment.',
     ),
     'thumbnail'    => 'logo.jpg',
-    'version'      => '4.0.0',
+    'version'      => '5.0.0',
     'author'       => 'OXID eSales AG',
     'url'          => 'http://www.oxid-esales.com',
     'email'        => 'info@oxid-esales.com',

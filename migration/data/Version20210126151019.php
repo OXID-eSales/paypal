dd70d65a7c00591c71f40e97f32fe5e7337c65e9
hkreuter <heike@reuter-consulting.org>
2021-02-11 15:32:03 +0100

    OXDEV-4245 OXDEV-4354 Add field to track paypal service type

--- migration/data/Version20210126151019.php
+++ migration/data/Version20210126151019.php
@@ -12,7 +12,10 @@ final class Version20210126151019 extends AbstractMigration
         $this->addSql("ALTER TABLE `oxuserbaskets`
           ADD COLUMN `OEPAYPAL_PAYMENT_TOKEN` char(32)
           character set latin1 collate latin1_general_ci NOT NULL DEFAULT ''
-          COMMENT 'Paypal payment token';");
+          COMMENT 'Paypal payment token',
+          ADD COLUMN `OEPAYPAL_SERVICE_TYPE` enum('1','2') NOT NULL default '1'
+          COMMENT 'Paypal service type - Standard = 1, Express Checkout = 2'
+          ;");
     }
 
     public function down(Schema $schema): void

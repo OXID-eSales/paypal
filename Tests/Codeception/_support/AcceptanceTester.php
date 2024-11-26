2af0bad5d0d69f60f0e842de1e8773e00275f06e
hkreuter <heike.reuter@oxid-esales.com>
2024-11-26 14:41:48 +0100

    Debug in GHA

--- Tests/Codeception/_support/AcceptanceTester.php
+++ Tests/Codeception/_support/AcceptanceTester.php
@@ -113,7 +113,7 @@ class AcceptanceTester extends \Codeception\Actor
     public function seePayPalInstallmentBanner()
     {
         $I = $this;
-
+$I->see('lalala');
         $I->waitForElement("//div[contains(@id, 'paypal-installment-banner-container')]//iframe");
         $I->switchToIFrame("//div[contains(@id, 'paypal-installment-banner-container')]//iframe");
         $I->waitForElementVisible("//body[node()]");
